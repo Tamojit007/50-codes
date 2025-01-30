53866c59e753b7e39edf259267e3ed62d3381f9d
Tamojit007 <tamojitmandal01@gmail.com>
2025-01-30 12:55:18 +0530

    done

--- src/arrayinput.java
+++ src/arrayinput.java
@@ -1,15 +1,9 @@
-
-import java.util.*;
+/**
+ * arrayinput
+ */
 public class arrayinput {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] =new int[n];
-        for(int i=0;i<n;i++){
-            a[i] = sc.nextInt();
-        }
-        for(int i=0;i<n;i++){
-            System.out.println(a[i]);
-        }
-    }
+ public static void main(String[] args) {
+    System.out.println("hello array");
+ }
+    
 }
